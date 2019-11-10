a1b11c34e2748a581955886bade28716f73f6c61
giovannic96 <giovanni.cala@hotmail.it>
2019-11-10 23:22:35 +0100

    removed useless link in fastPopulation.sql

--- config/database/fastPopulation.sql
+++ config/database/fastPopulation.sql
@@ -1,6 +1,3 @@
-http://localhost/Electronic-Student-Record-Management-System/public/
-
-
 //per caricare users
 insert into user values ('01', 'pino', 'topolino', 'pino@pino.it', 'Pino','PARENT', 0);
 insert into user values ('02', 'giulia', 'casale', 'giulia@giulia.it', 'Giulia','TEACHER', 0);
