5d47b7d4659b4afcd39dcf228844b83a401ed85f
fr2sinc <56112029+fr2sinc@users.noreply.github.com>
2019-11-10 22:53:59 +0100

    Copy and paste some user for fast population

---
+++ config/database/fastPopulation.sql
@@ -0,0 +1,9 @@
+http://localhost/Electronic-Student-Record-Management-System/public/
+
+
+//per caricare users
+insert into user values ('01', 'pino', 'topolino', 'pino@pino.it', 'Pino','PARENT', 0);
+insert into user values ('02', 'giulia', 'casale', 'giulia@giulia.it', 'Giulia','TEACHER', 0);
+insert into user values ('03', 'marco', 'canta', 'marco@marco.it', 'Marco','PARENT', 0);
+insert into user values ('04', 'alfredo', 'ponci', 'alfredo@alfredo.it', 'Alfredo','PARENT', 0);
+insert into user values ('05', 'luca', 'luca', 'luca@luca.it', 'Luca','PARENT', 0);
