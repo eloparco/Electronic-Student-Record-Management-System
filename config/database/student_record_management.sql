e3fa896b3904a40a6686a926b4abc48e9c91841f
enricol96 <enrico.loparco96@gmail.com>
2019-11-10 17:50:00 +0100

    Add databese with only USER table #35

---
+++ config/database/student_record_management.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 10, 2019 at 05:47 PM
+-- Server version: 5.7.27-0ubuntu0.19.04.1
+-- PHP Version: 7.2.24-0ubuntu0.19.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_record_management`
+--
+CREATE DATABASE IF NOT EXISTS `student_record_management` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `student_record_management`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `USER`
+--
+
+CREATE TABLE IF NOT EXISTS `USER` (
+  `SSN` varchar(50) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Surname` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `UserType` varchar(50) NOT NULL,
+  `AccountActivated` tinyint(1) NOT NULL,
+  PRIMARY KEY (`SSN`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
