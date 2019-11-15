d3cf996ce9fc517d5762f4db0716ee3c628cbd33
enricol96 <enrico.loparco96@gmail.com>
2019-11-15 23:50:26 +0100

    Update test script
    Now it starts mysql and apache. It also allow using codecept arguments

--- run_tests.sh
+++ run_tests.sh
@@ -6,8 +6,13 @@ user = root
 pass = 
 EOF
 
+# start apache and mysql
+service apache2 status &> /dev/null || sudo service apache2 start
+service mysql status &> /dev/null || sudo service mysql start
+
+# run tests
 java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.141.59.jar &
-php vendor/bin/codecept run 
+php vendor/bin/codecept run $1 $2
 kill $!
 
 # use production database
