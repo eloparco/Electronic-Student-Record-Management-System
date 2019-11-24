6a1b652691b162d19bea641c10a1eed047fdb763
sdiblasio <sdiblasio@github.com>
2019-11-24 15:29:39 +0100

    Fix foreign key constraints

--- config/database/student_record_management.sql
+++ config/database/student_record_management.sql
@@ -28,32 +28,42 @@ USE `student_record_management`;
 -- Add DROP statements to make changes to table definitions effective
 --
 
-DROP TABLE IF EXISTS `CHILD`;
+DROP TABLE IF EXISTS `ATTENDANCE`;
 DROP TABLE IF EXISTS `MARK`;
 DROP TABLE IF EXISTS `TOPIC`;
 DROP TABLE IF EXISTS `CLASS_TIMETABLE`;
 DROP TABLE IF EXISTS `TEACHER_SUBJECT`;
 DROP TABLE IF EXISTS `TIMETABLE`;
+DROP TABLE IF EXISTS `CHILD`;
 DROP TABLE IF EXISTS `SUBJECT`;
 DROP TABLE IF EXISTS `CLASS`;
+DROP TABLE IF EXISTS `USER_TYPE`;
 DROP TABLE IF EXISTS `USER`;
 
+
 --
--- Table structure for table `CHILD`
+-- Table structure for table `USER`
 --
 
-CREATE TABLE IF NOT EXISTS `CHILD` (
+CREATE TABLE IF NOT EXISTS `USER` (
   `SSN` char(16) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `Surname` varchar(50) NOT NULL,
-  `SSNParent1` char(16) NOT NULL,
-  `SSNParent2` char(16) NOT NULL,
-  `Class` char(2) NOT NULL,
-  PRIMARY KEY (`SSN`),
-  KEY `SSNParent1` (`SSNParent1`),
-  KEY `SSNParent2` (`SSNParent2`),
-  KEY `Class` (`Class`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `AccountActivated` tinyint(1) NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`SSN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `user_type`
+--
+
+CREATE TABLE IF NOT EXISTS `USER_TYPE` (
+  `SSN` char(16) NOT NULL,
+  `UserType` varchar(30) NOT NULL,
+  PRIMARY KEY (`SSN`,`UserType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -64,7 +74,47 @@ CREATE TABLE IF NOT EXISTS `CHILD` (
 CREATE TABLE IF NOT EXISTS `CLASS` (
   `Name` char(2) NOT NULL,
   PRIMARY KEY (`Name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SUBJECT`
+--
+
+CREATE TABLE IF NOT EXISTS `SUBJECT` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `HoursPerWeek` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TIMETABLE`
+--
+
+CREATE TABLE IF NOT EXISTS `TIMETABLE` (
+  `DayOfWeek` varchar(15) NOT NULL,
+  `StartHour` int(11) NOT NULL,
+  `EndHour` int(11) NOT NULL,
+  PRIMARY KEY (`DayOfWeek`,`StartHour`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TEACHER_SUBJECT`
+--
+
+CREATE TABLE IF NOT EXISTS `TEACHER_SUBJECT` (
+  `TeacherSSN` char(16) NOT NULL,
+  `SubjectID` int(11) NOT NULL,
+  `Class` char(2) NOT NULL,
+  PRIMARY KEY (`TeacherSSN`,`SubjectID`,`Class`),
+  KEY `SubjectID` (`SubjectID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -81,7 +131,26 @@ CREATE TABLE IF NOT EXISTS `CLASS_TIMETABLE` (
   PRIMARY KEY (`Class`,`DayOfWeek`,`StartHour`),
   KEY `DayOfWeek` (`DayOfWeek`,`StartHour`),
   KEY `TeacherSSN` (`TeacherSSN`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TOPIC`
+--
+
+CREATE TABLE IF NOT EXISTS `TOPIC` (
+  `Class` char(2) NOT NULL,
+  `Date` date NOT NULL,
+  `StartHour` int(11) NOT NULL,
+  `SubjectID` int(11) NOT NULL,
+  `TeacherSSN` char(16) NOT NULL,
+  `Title` varchar(50) NOT NULL,
+  `Description` varchar(400) NOT NULL,
+  PRIMARY KEY (`Class`,`Date`,`StartHour`),
+  KEY `SubjectID` (`SubjectID`),
+  KEY `TeacherSSN` (`TeacherSSN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -97,87 +166,48 @@ CREATE TABLE IF NOT EXISTS `MARK` (
   `Score` decimal(5,2) NOT NULL,
   PRIMARY KEY (`StudentSSN`,`SubjectID`,`Date`),
   KEY `SubjectID` (`SubjectID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Table structure for table `SUBJECT`
+-- Table structure for table `CHILD`
 --
 
-CREATE TABLE IF NOT EXISTS `SUBJECT` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(50) NOT NULL,
-  `HoursPerWeek` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `TEACHER_SUBJECT`
---
-
-CREATE TABLE IF NOT EXISTS `TEACHER_SUBJECT` (
-  `TeacherSSN` char(16) NOT NULL,
-  `SubjectID` int(11) NOT NULL,
-  `Class` char(2) NOT NULL,
-  PRIMARY KEY (`TeacherSSN`,`SubjectID`,`Class`),
-  KEY `SubjectID` (`SubjectID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `TIMETABLE`
---
-
-CREATE TABLE IF NOT EXISTS `TIMETABLE` (
-  `DayOfWeek` varchar(15) NOT NULL,
-  `StartHour` int(11) NOT NULL,
-  `EndHour` int(11) NOT NULL,
-  PRIMARY KEY (`DayOfWeek`,`StartHour`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `TOPIC`
---
-
-CREATE TABLE IF NOT EXISTS `TOPIC` (
-  `Class` char(2) NOT NULL,
-  `Date` date NOT NULL,
-  `StartHour` int(11) NOT NULL,
-  `SubjectID` int(11) NOT NULL,
-  `TeacherSSN` char(2) NOT NULL,
-  `Title` varchar(50) NOT NULL,
-  `Description` varchar(400) NOT NULL,
-  PRIMARY KEY (`Class`,`Date`,`StartHour`),
-  KEY `SubjectID` (`SubjectID`),
-  KEY `TeacherSSN` (`TeacherSSN`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `USER`
---
-
-CREATE TABLE IF NOT EXISTS `USER` (
+CREATE TABLE IF NOT EXISTS `CHILD` (
   `SSN` char(16) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `Surname` varchar(50) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Password` varchar(50) NOT NULL,
-  `UserType` varchar(30) NOT NULL,
-  `AccountActivated` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`SSN`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `SSNParent1` char(16) NOT NULL,
+  `SSNParent2` char(16) DEFAULT NULL,
+  `Class` char(2) DEFAULT NULL,
+  PRIMARY KEY (`SSN`),
+  KEY `SSNParent1` (`SSNParent1`),
+  KEY `SSNParent2` (`SSNParent2`),
+  KEY `Class` (`Class`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE IF NOT EXISTS `ATTENDANCE` (
+  `StudentSSN` char(16) NOT NULL,
+  `Date` date NOT NULL,
+  `Presence` enum('PRESENT','10_MIN_LATE','1_HOUR_LATE','ABSENT') DEFAULT NULL,
+  `ExitHour` int(11) NOT NULL DEFAULT 6
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `ATTENDANCE`
+  ADD CONSTRAINT `ATTENDANCE_ibfk_1` FOREIGN KEY (`StudentSSN`) REFERENCES `CHILD` (`SSN`);
 
 --
 -- Constraints for table `CHILD`
@@ -193,13 +223,16 @@ ALTER TABLE `CHILD`
 ALTER TABLE `CLASS_TIMETABLE`
   ADD CONSTRAINT `CLASS_TIMETABLE_ibfk_1` FOREIGN KEY (`Class`) REFERENCES `CLASS` (`Name`),
   ADD CONSTRAINT `CLASS_TIMETABLE_ibfk_2` FOREIGN KEY (`DayOfWeek`,`StartHour`) REFERENCES `TIMETABLE` (`DayOfWeek`, `StartHour`),
-  ADD CONSTRAINT `CLASS_TIMETABLE_ibfk_3` FOREIGN KEY (`TeacherSSN`) REFERENCES `USER` (`SSN`);
+  ADD CONSTRAINT `CLASS_TIMETABLE_ibfk_3` FOREIGN KEY (`TeacherSSN`) REFERENCES `USER` (`SSN`),
+  ADD CONSTRAINT `CLASS_TIMETABLE_ibfk_4` FOREIGN KEY (`SubjectID`) REFERENCES `SUBJECT`(`ID`);
 
 --
 -- Constraints for table `MARK`
 --
 ALTER TABLE `MARK`
-  ADD CONSTRAINT `MARK_ibfk_1` FOREIGN KEY (`SubjectID`) REFERENCES `SUBJECT` (`ID`);
+  ADD CONSTRAINT `MARK_ibfk_1` FOREIGN KEY (`SubjectID`) REFERENCES `SUBJECT` (`ID`),
+  ADD CONSTRAINT `MARK_ibfk_2` FOREIGN KEY (`Class`) REFERENCES `CLASS`(`Name`),
+  ADD CONSTRAINT `MARK_ibfk_3` FOREIGN KEY (`StudentSSN`) REFERENCES `CHILD`(`SSN`);
 
 --
 -- Constraints for table `TEACHER_SUBJECT`
@@ -215,6 +248,12 @@ ALTER TABLE `TOPIC`
   ADD CONSTRAINT `TOPIC_ibfk_1` FOREIGN KEY (`Class`) REFERENCES `CLASS` (`Name`),
   ADD CONSTRAINT `TOPIC_ibfk_2` FOREIGN KEY (`SubjectID`) REFERENCES `SUBJECT` (`ID`),
   ADD CONSTRAINT `TOPIC_ibfk_3` FOREIGN KEY (`TeacherSSN`) REFERENCES `USER` (`SSN`);
+
+--
+-- Constraints for table `user_type`
+--
+ALTER TABLE `USER_TYPE`
+  ADD CONSTRAINT `USER_TYPE_ibfk_1` FOREIGN KEY (`SSN`) REFERENCES `USER` (`SSN`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
