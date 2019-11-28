2ebe7fe12ddcd923b220d38a46d7349ee120a50b
sdiblasio <37545086+sdiblasio@users.noreply.github.com>
2019-11-28 16:48:56 +0100

    Update run_tests.py
    Edit python script for tests

--- run_tests.py
+++ run_tests.py
@@ -1,13 +1,15 @@
 import os
+import subprocess
+import sys
 
 with open('config/database/database.ini', 'w') as f:
 	f.write('host = localhost\n'
 		'name = student_record_management_test\n'
 		'user = root\n'
 		'pass = ')
-
-cmd = 'php vendor/bin/codecept run'
-os.system(cmd)
+os.environ["PATH"] += ";C:\\xampp\\mysql\\bin"
+cmd = 'vendor\\bin\\codecept.bat run ' + sys.argv[1] + ' ' + sys.argv[2]
+subprocess.check_call(cmd)
 
 with open('config/database/database.ini', 'w') as f:
 	f.write('host = localhost\n'
