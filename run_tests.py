6753b7bb1cc93fd957f253c265af4015ca3746f5
sdiblasio <sdiblasio@github.com>
2019-12-03 02:45:55 +0100

    Prepare python script to run tests #100

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import time
 
 with open('config/database/database.ini', 'w') as f:
 	f.write('host = localhost\n'
@@ -8,8 +9,12 @@ with open('config/database/database.ini', 'w') as f:
 		'user = root\n'
 		'pass = ')
 os.environ["PATH"] += ";C:\\xampp\\mysql\\bin"
-cmd = 'vendor\\bin\\codecept.bat run ' + sys.argv[1] + ' ' + sys.argv[2]
-subprocess.check_call(cmd)
+cmd = ['vendor\\bin\\codecept.bat', 'run', sys.argv[1], sys.argv[2], sys.argv[3]]
+bg_proc = "java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.141.59.jar"
+bg_service = subprocess.Popen(bg_proc, creationflags=subprocess.DETACHED_PROCESS)
+time.sleep(2) # be sure selenium is running
+subprocess.check_call(cmd, env=os.environ)
+bg_service.kill()
 
 with open('config/database/database.ini', 'w') as f:
 	f.write('host = localhost\n'
