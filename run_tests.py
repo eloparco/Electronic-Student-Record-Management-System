0a1d41d8b449a40591632ac69acb25de8727ace2
sdiblasio <sdiblasio@github.com>
2019-12-15 22:20:53 +0100

    Fixed run_tests.py:work with variable lenght args

--- run_tests.py
+++ run_tests.py
@@ -9,7 +9,9 @@ with open('config/database/database.ini', 'w') as f:
 		'user = root\n'
 		'pass = ')
 os.environ["PATH"] += ";C:\\xampp\\mysql\\bin"
-cmd = ['vendor\\bin\\codecept.bat', 'run', sys.argv[1], sys.argv[2]]
+cmd = ['vendor\\bin\\codecept.bat', 'run']
+for i in range(1, len(sys.argv)):
+	cmd.append(sys.argv[i])
 bg_proc = "java -jar -Dwebdriver.chrome.driver=.\chromedriver.exe selenium-server-standalone-3.141.59.jar"
 bg_service = subprocess.Popen(bg_proc, creationflags=subprocess.DETACHED_PROCESS)
 time.sleep(2) # be sure selenium is running
