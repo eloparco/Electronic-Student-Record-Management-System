cad107f484e445c262a597c441198461b27f0eee
enricol96 <enrico.loparco96@gmail.com>
2019-12-17 12:05:54 +0100

    Fix script to setup container using docker

--- setup.sh
+++ setup.sh
@@ -3,5 +3,4 @@ git clone https://github.com/enricol96/Electronic-Student-Record-Management-Syst
 service apache2 start
 service mysql start
 mysql -u root < /var/www/html/Electronic-Student-Record-Management-System/config/database/student_record_management.sql
-mysql -u root < /var/www/html/Electronic-Student-Record-Management-System/config/database/esrms_population.sql
 tail -f /dev/null
