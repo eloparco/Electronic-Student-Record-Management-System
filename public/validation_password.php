1f88be1f2d9e8db76fef0adadf262829dd829740
Marco Greco <marcoandreagreco@gmail.com>
2020-01-13 14:10:47 +0100

    Clean code smells

--- public/validation_password.php
+++ public/validation_password.php
@@ -1,28 +1,33 @@
 <?php
 include('utility.php');
 session_start();
-
+define("VAL_USRNAME", "username");
+define("VAL_OLD_PWD", "oldPassword");
+define("VAL_NEW_PWD","newPassword");
+define("PAGE_ADDR", "update_password.php");
+define("VAL_SESSION","mySession");
+define("CURR_USR_TYPE", "myUserType");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(isset($_POST['username']) && isset($_POST['oldPassword']) && isset($_POST['newPassword'])  && 
-        !empty($_POST['username']) && !empty($_POST['oldPassword']) && !empty($_POST['newPassword'])) {
+    if(isset($_POST[VAL_USRNAME]) && isset($_POST[VAL_OLD_PWD]) && isset($_POST[VAL_NEW_PWD])  && 
+        !empty($_POST[VAL_USRNAME]) && !empty($_POST[VAL_OLD_PWD]) && !empty($_POST[VAL_NEW_PWD])) {
 
-        $username = mySanitizeString($_POST['username']);
-        $oldPassword = mySanitizeString($_POST['oldPassword']);
-        $newPassword = mySanitizeString($_POST['newPassword']);
+        $username = mySanitizeString($_POST[VAL_USRNAME]);
+        $oldPassword = mySanitizeString($_POST[VAL_OLD_PWD]);
+        $newPassword = mySanitizeString($_POST[VAL_NEW_PWD]);
 
         $isPasswordCorrect1 = checkPassword($oldPassword);
         $isPasswordCorrect2 = checkPassword($newPassword);
         $isEmailCorrect = checkEmail($username);
 
         if(!$isEmailCorrect) {
-            redirect('update_password.php', EMAIL_INCORRECT); 
+            redirect(PAGE_ADDR, EMAIL_INCORRECT); 
         }
         else if(!$isPasswordCorrect1 && !$isPasswordCorrect2) {
-            redirect('update_password.php', PASSWORD_INCORRECT); 
+            redirect(PAGE_ADDR, PASSWORD_INCORRECT); 
         }
         else if($newPassword == $oldPassword) {
-            redirect('update_password.php', "The new password must be different from the current one");
+            redirect(PAGE_ADDR, "The new password must be different from the current one");
         } 
         else {
             $username = mySanitizeString($username);
@@ -36,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $dbUserType = $row['UserType'];  
                 $dbPassword = $row['Password'];
                 if ($dbPassword != $oldPassword || empty($dbPassword)) {//Password or email not valid
-                    redirect('update_password.php', "Invalid Username or Password"); 
+                    redirect(PAGE_ADDR, "Invalid Username or Password"); 
                 } else {                
                     if(!$result = mysqli_query($con,'UPDATE USER SET AccountActivated=1, Password="'.$newPassword.'" WHERE Email="'.$username.'" AND Password="'.$oldPassword.'";')) {
                         throw new UnexpectedValueException('update error');
@@ -44,46 +49,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     mysqli_close($con);
                     if($dbUserType == 'TEACHER') {
                         $_SESSION['time'] = time(); 
-                        $_SESSION['mySession'] = $username;
-                        $_SESSION['myUserType'] = 'TEACHER';
+                        $_SESSION[VAL_SESSION] = $username;
+                        $_SESSION[CURR_USR_TYPE] = 'TEACHER';
                         redirect ('user_teacher.php', '');
                     }
                     else if($dbUserType == 'PARENT'){
                         $_SESSION['time'] = time(); 
-                        $_SESSION['mySession'] = $username;
-                        $_SESSION['myUserType'] = 'PARENT';                        
+                        $_SESSION[VAL_SESSION] = $username;
+                        $_SESSION[CURR_USR_TYPE] = 'PARENT';                        
                         redirect ('user_parent.php', '');
                     }
                     else if($dbUserType == 'SECRETARY_OFFICER'){
                         $_SESSION['time'] = time(); 
-                        $_SESSION['mySession'] = $username;
-                        $_SESSION['myUserType'] = 'SECRETARY_OFFICER';
+                        $_SESSION[VAL_SESSION] = $username;
+                        $_SESSION[CURR_USR_TYPE] = 'SECRETARY_OFFICER';
                         redirect ('user_secretary.php', '');  
                     } else if($dbUserType == 'PRINCIPAL'){
                         $_SESSION['time'] = time(); 
-                        $_SESSION['mySession'] = $username;
-                        $_SESSION['myUserType'] = 'PRINCIPAL';
+                        $_SESSION[VAL_SESSION] = $username;
+                        $_SESSION[CURR_USR_TYPE] = 'PRINCIPAL';
                         redirect ('user_principal.php', '');  
                     } else if($dbUserType == 'SYS_ADMIN'){
                         $_SESSION['time'] = time(); 
-                        $_SESSION['mySession'] = $username;
-                        $_SESSION['myUserType'] = 'SYS_ADMIN';
+                        $_SESSION[VAL_SESSION] = $username;
+                        $_SESSION[CURR_USR_TYPE] = 'SYS_ADMIN';
                         redirect ('user_admin.php', '');  
                     } else {
-                        redirect('update_password.php', LOGIN_USER_NOT_DEFINED);  
+                        redirect(PAGE_ADDR, LOGIN_USER_NOT_DEFINED);  
                     }   
             } 
                 
             }catch (UnexpectedValueException $e) {                
                 mysqli_close($con);
-                redirect('update_password.php', "Something went wrong, retry Dberror");
+                redirect(PAGE_ADDR, "Something went wrong, retry Dberror");
             }               
 
         }
     } else {
-        redirect('update_password.php', "Something went wrong, retry");    
+        redirect(PAGE_ADDR, "Something went wrong, retry");    
     }
 } else {
-    redirect('update_password.php', "Something went wrong, retry"); 
+    redirect(PAGE_ADDR, "Something went wrong, retry"); 
 }
 ?>
