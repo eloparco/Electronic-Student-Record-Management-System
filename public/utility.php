fd732f413e7f633b4ce1b4682b899860b1b1201c
enricol96 <enrico.loparco96@gmail.com>
2020-01-14 13:42:46 +0100

    Fix vulnerability #207

--- public/utility.php
+++ public/utility.php
@@ -1416,7 +1416,7 @@ function https_redirect() {
     session_start();
 
     if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
-        $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        $location = 'https://' . mySanitizeString($_SERVER['HTTP_HOST']) . mySanitizeString($_SERVER['REQUEST_URI']);
         header('HTTP/1.1 301 Moved Permanently');
         header(LOCATION_LABEL.$location);
         exit;
